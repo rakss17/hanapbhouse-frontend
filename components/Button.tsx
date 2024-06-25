1b04fdd0da71767c9bc12a708fb7ef34a8beacad
Bohari S. Ambulo <bohariambulo17@gmail.com>
2024-06-25 22:09:23 +0800

    added loading props to button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity, Text, ActivityIndicator, View } from "react-native";
 import { ButtonProps } from "@/interfaces/ButtonProps";
 
 export const Button = React.forwardRef<TouchableOpacity, ButtonProps>(
-  ({ text, text2, onPress, buttonStyle, textStyle }, ref) => {
+  (
+    {
+      text,
+      text2,
+      onPress,
+      buttonStyle,
+      textStyle,
+      isLoading,
+      loadingColor,
+      loadingSize,
+      loadingText,
+      disabled,
+    },
+    ref
+  ) => {
     return (
-      <TouchableOpacity ref={ref} onPress={onPress} style={buttonStyle}>
-        <Text style={textStyle}>{text}</Text>
+      <TouchableOpacity
+        ref={ref}
+        disabled={disabled}
+        onPress={onPress}
+        style={buttonStyle}
+      >
+        {isLoading ? (
+          <View style={{ flexDirection: "row", gap: 5 }}>
+            <Text style={textStyle}>{loadingText}</Text>
+            <ActivityIndicator color={loadingColor} size={loadingSize} />
+          </View>
+        ) : (
+          <Text style={textStyle}>{text}</Text>
+        )}
+
         {text2 && <Text style={textStyle}>{text2}</Text>}
       </TouchableOpacity>
     );
