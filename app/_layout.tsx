a273cffdccf6f9313d55cc0a259b05ba41927747
Bohari S. Ambulo <bohariambulo17@gmail.com>
2024-06-26 20:44:23 +0800

    added icon and adjusted margin top of the Toast Provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from "expo-router";
 import { ToastProvider } from "react-native-toast-notifications";
+import { Text } from "react-native";
+import { Viewport } from "@/styles/styles";
 
 export default function RootLayout() {
   return (
-    <ToastProvider>
+    <ToastProvider
+      icon={<Text>👋</Text>}
+      style={{ top: Viewport.height * 0.03 }}
+    >
       <Stack initialRouteName="(auth)" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" />
         <Stack.Screen name="(tabs)" />
